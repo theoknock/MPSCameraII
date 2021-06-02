d81afe0325ca3727926bb73c41872df26ded62e3
James Alan Bush <jamesalanbush1972@gmail.com>
2021-06-02 11:50:34 -0400

    Initial Commit

---
+++ iOSGameTemplate/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  iOSGameTemplate
+//
+//  Created by Xcode Developer on 6/2/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
